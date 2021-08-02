af7b5b9359123ff6bb39bd177c89857234d82db3
w1ckedd <alirezachoobineh@ymail.com>
2021-08-02 11:08:32 +0430

    updated images models and added serializers

--- books/serializers.py
+++ books/serializers.py
@@ -2,6 +2,8 @@ from authors.serializers import AuthorSerializer
 from rest_framework import serializers
 from .models import Book, Genre
 from authors.models import Author
+from images.models import Photo
+from images.serializers import PhotoSerializer
 
 class GenreSerializer(serializers.ModelSerializer):
   class Meta:
@@ -9,8 +11,9 @@ class GenreSerializer(serializers.ModelSerializer):
     fields = ('title',)
 
 class BookSerializer(serializers.ModelSerializer):
+  images = PhotoSerializer(many=True)
   genres = serializers.StringRelatedField(many=True, read_only=True)
   author = serializers.StringRelatedField(read_only=True)
   class Meta:
     model = Book
-    fields = '__all__'
+    fields = ('id', 'genres', 'title', 'author', 'images', 'description', 'pages')
--- images/models.py
+++ images/models.py
@@ -5,9 +5,9 @@ from users.models import User
 
 class Photo(models.Model):
   image = models.ImageField(upload_to='photos/%Y/%m/%d/')
-  user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
-  author = models.ForeignKey(Author, blank=True, null=True, on_delete=models.CASCADE)
-  book = models.ForeignKey(Book, blank=True, null=True, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='images')
+  author = models.ForeignKey(Author, blank=True, null=True, on_delete=models.CASCADE, related_name='images')
+  book = models.ForeignKey(Book, blank=True, null=True, on_delete=models.CASCADE, related_name='images')
 
   def __str__(self):
     return self.image.url
